9ba368f0f74b488cd2d98e170807bfe0fd1250d4
Marv Adesanya <marvellousniyi123@gmail.com>
2023-07-06 00:04:30 +0100

    CORS update

--- backend/src/config/allowedOrigins.ts
+++ backend/src/config/allowedOrigins.ts
@@ -1,8 +1,6 @@
 const allowedOrigins: string[] = [
-  "https://www.yoursite.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
   "http://localhost:5173",
+  "https://inbranded-z161.vercel.app/",
 ];
 
 export default allowedOrigins;
